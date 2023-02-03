5b819475b429d3cf09db8d577c30efb61291f903
davidreyesd13 <davidreyesd13@gmail.com>
2023-02-03 12:51:59 -0600

    Inicio

---
+++ src/main/java/com/mycompany/lab3_davidreyes/Lab3_DavidReyes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab3_davidreyes;
+
+/**
+ *
+ * @author david
+ */
+public class Lab3_DavidReyes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
